9d8462f5fbda4534a9e457bdc6ec9bd8541cbbe3
Abukar12 <69994122+Abukar12@users.noreply.github.com>
2020-08-28 18:19:10 -0400

    Initial commit

---
+++ Seller.py
@@ -0,0 +1,22 @@
+# import csv
+# data = []
+# csvfile = open('sell.csv')
+# reader = csv.reader(csvfile)
+# for row in reader:
+#     data.append(row)
+# row = [x[0] for x in data]
+# stock = row[0]
+# print(stock)
+print("")
+print('WOII____,')
+print("")    
+print ("Below are my numbers this week:")
+print("")
+print("X:1")
+print("E:O")
+print("UN:0")
+print("SOS:0")
+print("")
+print("Regards,")
+print('')
+print("Sgt Abukar")
