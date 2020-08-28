afacadf1198592684f48276e9448e4da61ceeb16
Abukar12 <69994122+Abukar12@users.noreply.github.com>
2020-08-28 14:06:57 -0400

    Add files via upload

--- stock screener2.py
+++ stock screener2.py
@@ -1,15 +1,12 @@
-API_Key_1: '1354e1035b2d404c8ded01d402571d6a'
-API_Key_2: '61a5842fb2f64893ba1ec203b005d3b2'
-API_Key_3: '644ccd59e83f4d23970afc6715f3d37a'
-API_Key_4: '7526755fae07412cb6b20b27acb4321c' 
-API_Key_5: 'c9d9047973304487b936445bad3e63d9'
+#API Key 1: 1354e1035b2d404c8ded01d402571d6a
+#API Key 2:61a5842fb2f64893ba1ec203b005d3b2
+#API Key 3: 644ccd59e83f4d23970afc6715f3d37a
 from twelvedata import TDClient
 import csv
 import time
 
 MAX_INTREVAL = 2
 data = []
-
 with open('companylists.csv') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
@@ -27,13 +24,17 @@ def pull_from_frame(data, index, intreval):
     pulled = data.filter(regex=index).values.tolist()[intreval - 1][0]
     return pulled
 
-td = TDClient(apikey="1354e1035b2d404c8ded01d402571d6a")
 
-# data client 1100
-for ticker in row:
+# data client
+for i in range(1,136):
+    ticker = i
+
     
+    Stock = (row[ticker])
+    td = TDClient(apikey="644ccd59e83f4d23970afc6715f3d37a")
+
     ts = td.time_series(
-        symbol=ticker,
+        symbol=Stock,
         interval="1day",
         outputsize=MAX_INTREVAL,
         timezone="America/New_York",
@@ -41,18 +42,10 @@ for ticker in row:
     # end_date="YYYY-MM-DD"
 )
 
-if ticker == 800:
-    API = API_Key_2
-if ticker == 1600:
-    API = API_Key_3
-if ticker == 2400:
-    API = API_Key_4
-if ticker == 3200:
-    API = API_Key_5
-
 # Tech Indcators
-    bands = ts.with_bbands(time_period=100).as_pandas()
-    mas = ts.with_stddev().without_ohlc().as_pandas()
+    bands = ts.with_bbands(time_period=800).as_pandas()
+    slows = ts.with_stoch().without_ohlc().as_pandas()
+    mas = ts.with_sma(time_period=800).without_ohlc().as_pandas()
 # pull from frame
     open = p(bands, 'open', 1)
     open2 = p(bands, 'open', MAX_INTREVAL)
@@ -69,6 +62,12 @@ if ticker == 3200:
     volume = p(bands, 'volume', 1)
     volume2 = p(bands, 'volume', MAX_INTREVAL)
 
+    slow_k = p(slows, 'slow_k', 1)
+    slow_k2 = p(slows, 'slow_k', MAX_INTREVAL)
+
+    slow_d = p(slows, 'slow_d', 1)
+    slow_d2 = p(slows, 'slow_d', MAX_INTREVAL)
+
     low_band = p(bands, 'lower_band', 1)
     low_band2 = p(bands, 'lower_band', MAX_INTREVAL)
 
@@ -78,40 +77,49 @@ if ticker == 3200:
     high_band = p(bands, 'upper_band', 1)
     high_band2 = p(bands, 'upper_band', MAX_INTREVAL)
 
-    stddev = p(mas, 'stddev', 1)
-    stddev2 = p(mas, 'stddev', MAX_INTREVAL)
+    sma = p(mas, 'sma', 1)
+    sma2 = p(mas, 'sma', MAX_INTREVAL)
 
     score = 0
 # buy condition
-    if close < 40:
+    if slow_d and slow_k < 20:
+        score = score + 3
+
+    if slow_d and slow_k < 40:
+        score = score + 3
+
+    if slow_k > slow_d and (slow_k + slow_d) < 100:
+        score = score + 7
+
+    if (close2 / close * 100 - 100) < -5:
         score = score + 5
 
-    if close < 20:
-        score = score + 5     
+    if (close2 / close * 100 - 100) < -10:
+        score = score + 2
 
-    if volume > 17500000:
-        score = score + 5
-    
-    if (low / high * 100 - 100) < 2.5:
-        score = score + 5 
-    
-    if stddev > 5:
-        score = score + 10
-    
-    if stddev > 10:
+    if (sma / close * 100 - 100) < -3:
+        score = score + 3
+
+    if (volume2 / volume * 100 - 100) < 5:
         score = score + 5
 
-    if stddev > 20:
-        score = score + 5
+    if (sma / close * 100 - 100) < -7:
+        score = score + 2
+
+    if (close / low_band * 100 - 100) < 15:
+        score = score - 5
+    
+
+    
 
 
     print("")
     print("")
-    print("Stock's score is", score,"/ 40")
+    print("Stock's score is", score,"/ 35")
     print("")
    
 
-    if score > 19 and score < 30: 
+    if score > 24 and score < 30: 
         f = (Stock)
         gs.append(f)
         print(f'Good Stock Tickers are {gs}')
@@ -121,14 +129,15 @@ if ticker == 3200:
         grs.append(f)
         print(f'great Stock Tickers are {grs}')
 
-    time.sleep(15)
-    #if i == 3:
-     #   break
+    
+    #if i == 2:
+        #break
     print(i)
-
+    time.sleep(20)
 print("")
 print("")
 print(f'The Good Stock Tickers Are {gs}')
 print(f'The Great Stock Tickers Are {grs}')
 
+    
 
